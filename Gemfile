b3924afb7a4ecff899aea0e0fc249d0857824cc0
Matthew Guilliams <matthew.guilliams@gmail.com>
2014-09-25 01:39:36 +0000

    Decimal entry input and beginning ajax entry create

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '4.0.5'
 gem 'roo'
 gem 'bootstrap-sass'
 gem 'money-rails'
+gem 'pry'
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,7 @@
 class EntriesController < ApplicationController
 
   def index
+    @entry = Entry.new
     @register = Register.find(params[:register_id])
 
     @entries = @register.entries.where("date >= ?", 6.months.ago).order(date: :asc)
@@ -12,4 +13,35 @@ class EntriesController < ApplicationController
     @cleared_balance = @available_balance - Money.new(differential)
 
   end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    @register = Register.find(params[:register_id])
+    @entry = @register.entries.build(entry_params)
+
+    debit = params[:debit].to_money
+    @entry.debit_cents = debit.cents
+
+    credit = params[:credit].to_money
+    @entry.credit_cents = credit.cents
+
+    @entry.save
+
+    respond_to do |format|
+      format.js do
+        render "create"
+      end
+    end
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:name, :date, :credit_cents, :debit_cents)
+  end
 end
+
+
